886525cb314e0840aa3def1b11a5abd97b54ddf9
⚡️Nefe⚡️ <Just4Ease@users.noreply.github.com>
2025-11-24 13:25:32 +0300

    feat: updated proto to test

--- _labs/srv/srv.go
+++ _labs/srv/srv.go
@@ -11,6 +11,28 @@ import (
 type CustomAccountsService struct {
 }
 
+func (c CustomAccountsService) StreamWalletUpdatesFromServer(request *accounts.GetByIdRequest, server accounts.AccountService_StreamWalletUpdatesFromServerServer) error {
+	list := FakeWallets(2000)
+	for _, w := range list {
+		if err := server.Send(w); err != nil {
+			return err
+		}
+		time.Sleep(1 * time.Second)
+	}
+	return nil
+}
+
+func (c CustomAccountsService) StreamWalletUpdatesFromClient(server accounts.AccountService_StreamWalletUpdatesFromClientServer) error {
+	for {
+		in, err := server.Recv()
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(in.Id)
+	}
+}
+
 func (c CustomAccountsService) CreateWallet(ctx context.Context, req *accounts.CreateWalletRequest) (*accounts.Wallet, error) {
 	wallet := &accounts.Wallet{
 		Id:       fmt.Sprintf("wlt_%d", time.Now().Unix()),
